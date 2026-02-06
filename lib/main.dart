2c0a1b2060e2c4f29f859fc02ebcc494dca4fe5f
YusraBadran <yusrabadran1225@gmail.com>
2026-02-07 01:39:05 +0300

    Scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:exercises/pages/PopupMenuButton.dart';
 import 'package:exercises/pages/SearchDelegate.dart';
 import 'package:exercises/pages/about.dart';
-import 'package:exercises/pages/home.dart';
-import 'package:exercises/pages/list.dart';
 import 'package:exercises/pages/pageView.dart';
 import 'package:exercises/pages/showDialog.dart';
 import 'package:exercises/pages/BottomSheet.dart';
---
+++ lib/pages/scroll.dart
@@ -0,0 +1,13 @@
+import 'package:/flutter/material.dart';
+
+class ScrollPage extends StatefulWidget {
+  @override
+  _ScrollPageState createState() => _ScrollPageState();
+}
+
+class _ScrollPageState extends State<ScrollPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
